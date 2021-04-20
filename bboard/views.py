930b1014ad2745eacab3b21c0c1d7e1cec0ed584
Alehkod <kodikoleg@gmail.com>
2021-04-20 21:57:14 +0300

    Add rubric

--- bboard/views.py
+++ bboard/views.py
@@ -9,10 +9,8 @@ from .models import Bb
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
 
 
 def cur_time(request):
